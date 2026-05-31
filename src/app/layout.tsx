0f4c449ba00d6fad7d5a84cdf925db0231a99ac3
Azeem-097 <azeem.ntu.edu@gmail.com>
2026-05-31 17:25:41 +0500

    Updated website changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+// @ts-expect-error CSS import handled by Next.js
 import "./globals.css";
 import GoogleAnalytics from "@/components/GoogleAnalytics";
 import SchemaOrg from "@/components/SchemaOrg";
